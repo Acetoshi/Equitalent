c886eb00bd682f187937d2618c8f660aaf82b055
TheoDegorre <degorret@gmail.com>
2024-07-03 21:41:02 +0200

    fetch of an api for analsysis component and begin of a services for analysis components

--- src/app/analysis/analysis.component.ts
+++ src/app/analysis/analysis.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from "../analysis/analysis";
 
 @Component({
-  selector: 'app-analysis',
   standalone: true,
-  imports: [],
+  selector: 'app-score',
   templateUrl: './analysis.component.html',
-  styles: ``
+  styleUrls: ['./analysis.component.css']
 })
 
 export class AnalysisComponent implements OnInit {
-  analysisResult = ANALYSIS;
+  title = 'HTTP using native fetch API';
+  url: string = 'https://type.fit/api/quotes';
+  quotes: Quote[] = [];
 
-  ngOnInit(): void {
-    console.table(this.analysisResult);
+  ngOnInit() {
+    console.log("coucou je suis oninit")
+    fetch(this.url)
+      .then(response => response.json())
+      .then((data : Quote[]) => {
+        this.quotes=data;
+      })
+      .then(() => console.log("ceci est fetch", this.quotes));
   }
-
 }
+
