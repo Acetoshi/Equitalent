5ccf7f6d17bae39961d4413c362e877f7d95ee89
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-07-03 17:31:05 +0200

    added minimal CSS to evaluation page

--- src/app/evaluation/evaluation.component.ts
+++ src/app/evaluation/evaluation.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
   standalone: true,
   imports: [],
   templateUrl:'./evaluation.component.html',
-  styles: ``,
+  styleUrl:'./evaluation.component.css',
 })
 export class EvaluationComponent {
   cvs = [
