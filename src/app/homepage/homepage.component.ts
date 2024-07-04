0833b74844f96ae3693fc0ae2eaf04e2178fac66
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-07-04 12:03:50 +0200

    started to work on homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
-  selector: 'app-homepage',
+  selector: "app-homepage",
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   template: `
-    <p>
-      homepage works!
-    </p>
+    <main class="homepage">
+      <h2>E QUITALENT</h2>
+      <h1>"Serez-vous le meilleur RH ?"</h1>
+      <a
+        class="button-accent button-accent-dark"
+        routerLink="/evaluation"
+        draggable="false"
+        >Trouvez les CV qui matchent le mieux avec l'annonce !</a
+      >
+    </main>
   `,
-  styles: ``
+  styleUrls: [`./homepage.component.css`],
 })
-export class HomepageComponent {
-
-}
+export class HomepageComponent {}
